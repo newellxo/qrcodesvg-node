40c1abb9605ab878cc4d7d3058d8122b3009c38a
Zhang <zchning@gmail.com>
2018-06-18 15:22:12 -0400

    add png as a return option

--- examples/server/index.js
+++ examples/server/index.js
@@ -2,7 +2,25 @@ const fs = require('fs')
 const Qrcodesvg = require('../../lib/index.js')
 
 const qrcode = new Qrcodesvg('Hello', 400, { ecclevel: 2, eye: 'circle' })
-const data = qrcode.generate(
+// const data = qrcode.generate(
+//   {
+//     method: 'circle',
+//     'fill-colors': ['#00000'],
+//     radius: 5
+//   },
+//   {
+//     'stroke-width': 0.00001
+//   }
+// )
+
+// fs.writeFile('./output.svg', data, function (err) {
+//   if (err) {
+//     return console.log(err)
+//   }
+//   console.log('The file was saved!')
+// })
+
+const data = qrcode.generatePNG(
   {
     method: 'circle',
     'fill-colors': ['#00000'],
@@ -10,10 +28,12 @@ const data = qrcode.generate(
   },
   {
     'stroke-width': 0.00001
+  }, {
+    width: 400,
+    height: 400
   }
 )
-
-fs.writeFile('./output.svg', data, function (err) {
+fs.writeFile('./output.png', data, function (err) {
   if (err) {
     return console.log(err)
   }
