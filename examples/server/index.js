7073c0fbc4407bd2e75e9e79aacf989516b98391
Zhang <zchning@gmail.com>
2018-06-18 16:28:27 -0400

    add async method to generate qrcode

--- examples/server/index.js
+++ examples/server/index.js
@@ -2,25 +2,28 @@ const fs = require('fs')
 const Qrcodesvg = require('../../lib/index.js')
 
 const qrcode = new Qrcodesvg('Hello', 400, { ecclevel: 2, eye: 'circle' })
-// const data = qrcode.generate(
-//   {
-//     method: 'circle',
-//     'fill-colors': ['#00000'],
-//     radius: 5
-//   },
-//   {
-//     'stroke-width': 0.00001
-//   }
-// )
 
-// fs.writeFile('./output.svg', data, function (err) {
-//   if (err) {
-//     return console.log(err)
-//   }
-//   console.log('The file was saved!')
-// })
+// Generate SVG
+const svgData = qrcode.generate(
+  {
+    method: 'circle',
+    'fill-colors': ['#00000'],
+    radius: 5
+  },
+  {
+    'stroke-width': 0.00001
+  }
+)
 
-const data = qrcode.generatePNG(
+fs.writeFile('./output.svg', svgData, function (err) {
+  if (err) {
+    return console.log(err)
+  }
+  console.log('The file was saved!')
+})
+
+// // Generate png
+const PngData = qrcode.generatePNG(
   {
     method: 'circle',
     'fill-colors': ['#00000'],
@@ -33,9 +36,26 @@ const data = qrcode.generatePNG(
     height: 400
   }
 )
-fs.writeFile('./output.png', data, function (err) {
+fs.writeFile('./output.png', PngData, function (err) {
   if (err) {
     return console.log(err)
   }
   console.log('The file was saved!')
 })
+
+// Generate PNG with callback
+qrcode.generatePNG(
+  {
+    method: 'circle',
+    'fill-colors': ['#00000'],
+    radius: 5
+  },
+  {
+    'stroke-width': 0.00001
+  }, {
+    width: 400,
+    height: 400
+  }, (err, data) => {
+    console.log(err, data)
+  }
+)
